c4e4d88297bdacb2ca4eca3ade2e1c70b7ef21b0
Samuel Cornet <samuel@noukod.com>
2019-01-07 17:03:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,28 +2,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+
   get '/reversename/:name' do
     name.each do |char|
       name.unshift(char)
       "#{params[:name]}"
     end
   end
-  
+
   get '/square/:number' do
     "#{(params[:number].to_i ** 2).to_s}"
   end
-  
+
   get '/say/:number/:phrase' do
     "#{params[:phrase] * params[:number].to_i}"
   end
-  
+
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
-  
+
   get '/:operation/:number1/:number2' do
-    @num1, @num2, @op = params[:number1].to_i, params[:number2].to_i, params[:operation]
+    @number1, @number2, @operation = params[:number1].to_i, params[:number2].to_i, params[:operation]
     case @op
     when 'add'
       "#{@num1 + @num2}"
